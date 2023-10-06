bd7418be250b1adc03b990d48267e33701241f06
Bartosz Tanski <b.t4nsky@gmail.com>
2023-10-06 09:34:06 +0200

    initializr

---
+++ src/main/java/com/bartosztanski/visitreservation/VisitReservationApplication.java
@@ -0,0 +1,13 @@
+package com.bartosztanski.visitreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bartosztanski/visitreservation/VisitReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bartosztanski.visitreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
