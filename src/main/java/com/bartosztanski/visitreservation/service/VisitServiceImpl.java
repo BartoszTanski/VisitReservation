0ea6b9efccd66a743a619a564a42890d9c9bc59a
Bartosz Tanski <b.t4nsky@gmail.com>
2023-10-06 13:10:43 +0200

    small visitService.findById changes

--- src/main/java/com/bartosztanski/visitreservation/service/VisitServiceImpl.java
+++ src/main/java/com/bartosztanski/visitreservation/service/VisitServiceImpl.java
@@ -1,14 +1,23 @@
 package com.bartosztanski.visitreservation.service;
 
+import com.bartosztanski.visitreservation.entity.VisitEntity;
 import com.bartosztanski.visitreservation.model.VisitRequest;
 import com.bartosztanski.visitreservation.model.VisitResponse;
+import com.bartosztanski.visitreservation.repository.VisitRepository;
 
 public class VisitServiceImpl implements VisitService{
-
+	
+	private final VisitRepository visitRepository;
+	
+	public VisitServiceImpl(VisitRepository visitRepository) {
+		this.visitRepository = visitRepository;
+	}
+	
 	@Override
 	public VisitResponse getVisitById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		VisitEntity visit = visitRepository.findById(id).get();
+		VisitResponse response = null; //todo 
+		return response;
 	}
 
 	@Override
