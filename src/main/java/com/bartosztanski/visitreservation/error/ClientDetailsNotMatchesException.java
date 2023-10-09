ab53420e452b4a260bb0365e505bbb0e617dd028
Bartosz Tanski <b.t4nsky@gmail.com>
2023-10-09 19:55:15 +0200

    error classes - empty yet

---
+++ src/main/java/com/bartosztanski/visitreservation/error/ClientDetailsNotMatchesException.java
@@ -0,0 +1,10 @@
+package com.bartosztanski.visitreservation.error;
+
+public class ClientDetailsNotMatchesException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8363296284078335551L;
+
+}
---
+++ src/main/java/com/bartosztanski/visitreservation/error/VisitNotAvailableException.java
@@ -0,0 +1,10 @@
+package com.bartosztanski.visitreservation.error;
+
+public class VisitNotAvailableException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3706262616960534362L;
+
+}
